ab20392186fa573e791658feb442d1f87b38e865
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-02-26 16:08:44 +0000

    catch windows error in supports_ansi

--- utility/terminal.py
+++ utility/terminal.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from typing import TextIO
 
+import ctypes
 import sys
 
 from .typing import MISSING
@@ -22,8 +23,13 @@ def supports_ansi(
     """
 
     if sys.platform == "win32":
-        if isinstance(console_mode := get_console_mode(stream), OutputConsoleMode):
-            return bool(console_mode & OutputConsoleMode.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
+        try:
+            console_mode = get_console_mode(stream)
+        except ctypes.WinError:
+            return False
+        else:
+            if isinstance(console_mode, OutputConsoleMode):
+                return bool(console_mode & OutputConsoleMode.ENABLE_VIRTUAL_TERMINAL_PROCESSING)
 
     if stream is MISSING:
         stream = sys.stdout
