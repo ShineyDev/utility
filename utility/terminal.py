c94c1e99e94b4c2343470ea0bd57f471f24b567f
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-02-26 16:17:09 +0000

    implement wants_ansi

--- utility/terminal.py
+++ utility/terminal.py
@@ -5,6 +5,7 @@ if TYPE_CHECKING:
     from typing import TextIO
 
 import ctypes
+import os
 import sys
 
 from .typing import MISSING
@@ -37,6 +38,60 @@ def supports_ansi(
     return stream.isatty()
 
 
+def wants_ansi(
+    stream: TextIO = MISSING,
+    /,
+) -> bool:
+    """
+    Determines whether a stream advertises that it both wants and
+    supports ANSI escape sequences to be used in its output.
+
+    Specifically, this function does the following, in this order:
+
+    - If the stream advertises that it wants ANSI escape sequences
+      continue, otherwise return False.
+        - On Python 3.13 and higher, if the environment variable
+          |PYTHON_COLORS| is set to ``"0"`` return False.
+        - If the environment variable |NO_COLOR| is set with any value
+          return False.
+        - If the environment variable |FORCE_COLOR| is set with any
+          value return True.
+        - If the environment variable "TERM" is set to ``"dumb"``
+          return False.
+    - If the stream advertises that it supports ANSI escape sequences
+      return True, otherwise return False.
+
+
+    Parameters
+    ----------
+    stream: :class:`~io.TextIO`
+        A :class:`text <str>` stream.
+
+
+    Returns
+    -------
+    :class:`bool`
+        Whether the stream advertises that it both supports and wants
+        ANSI escape sequences to be used in its output.
+    """
+
+    if sys.version_info >= (3, 13):
+        if os.environ.get("PYTHON_COLORS") == "0":
+            return False
+
+    if "NO_COLOR" in os.environ.keys():
+        return False
+
+    if "FORCE_COLOR" in os.environ.keys():
+        return True
+
+    if os.environ.get("TERM") == "dumb":
+        return False
+
+    return supports_ansi(stream)
+
+
 __all__ = [
     "supports_ansi",
+    "wants_ansi",
 ]
