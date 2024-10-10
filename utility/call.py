ad845c08b5154b1d98461c3ac47624ee4122294e
Riley Shaw <30989490+ShineyDev@users.noreply.github.com>
2024-10-10 13:49:00 +0100

    implement bind_function
    Co-authored-by: bijij <8677174+bijij@users.noreply.github.com>

--- utility/call.py
+++ utility/call.py
@@ -4,12 +4,21 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from collections.abc import Awaitable, Callable
     from typing import Any, TypeVar
-    from typing_extensions import ParamSpec
+    from typing_extensions import Concatenate, ParamSpec
 
     _P = ParamSpec("_P")
     _T = TypeVar("_T")
+    _R = TypeVar("_R")
 
 import inspect
+
+
+def bind_function(
+    callable: Callable[Concatenate[_T, _P], _R],
+    instance: _T,
+    /,
+) -> Callable[_P, _R]:
+    return callable.__get__(instance, instance.__class__)
 
 
 async def call_maybe_coroutine(
@@ -26,5 +35,6 @@ async def call_maybe_coroutine(
 
 
 __all__ = [
+    "bind_function",
     "call_maybe_coroutine",
 ]
